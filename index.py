fd476b8a5315167291e1c9c1d625a4626253e996
htethtethtunjewel <htethtethtunhtet@gmail.com>
2018-06-19 19:51:14 +0630

    Flask Chapter-1

--- index.py
+++ index.py
@@ -2,10 +2,10 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home ():
-      return "Hello World"
+    return "Hello World"
 @app.route("/index")
 def index():
-      return "This is index"
+    return "This is index"
 if __name__=="__main__":
       app.run(debug=True)
     
